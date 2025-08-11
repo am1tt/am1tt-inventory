7851a51df90a23d102eb297e8d1a83087c629c0e
am1tt <eflatmajor811@gmail.com>
2025-08-11 19:16:55 +0530

    [ added ] load_subdomains() for faster file handling | will update pending check_domains shortly

--- scripts /subdomain_fuzzer/subdomain_fuzzer.py
+++ scripts /subdomain_fuzzer/subdomain_fuzzer.py
@@ -2,23 +2,32 @@
 import requests
 
 
+
+def load_subdomains(payload_file):
+    try:
+        with open(payload_file,'r') as f:
+            return f.read().splitlines()
+    
+    except FileNotFoundError:
+        print(f"[!] file not found : {payload_file}")
+        return []
+
+    
+
 def check_subdomains(domain,payload):
-    with open(payload, 'r') as f:
 
-        subdomains = f.read().splitlines() 
+            for sub in subdomains: 
+                for protocol in ["http","https"]: 
 
-        for sub in subdomains: 
-            for protocol in ["http","https"]: 
+                    url = f"{protocol}://{sub}.{domain}"
 
-                url = f"{protocol}://{sub}.{domain}"
-
-                try:
-                    response = requests.get(url,timeout=2)
-                    print(f"[+] {url} - status : {response.status_code}")
-                except requests.ConnectionError:
-                    print(f"[-] {url} - failed to connect") 
-                except requests.Timeout: 
-                    print(f"[-] {url} - Request timed out")
+                    try:
+                        response = requests.get(url,timeout=2)
+                        print(f"[+] {url} - status : {response.status_code}")
+                    except requests.ConnectionError:
+                        print(f"[-] {url} - failed to connect") 
+                    except requests.Timeout: 
+                        print(f"[-] {url} - Request timed out")
     
 
 domain = "google.com"
