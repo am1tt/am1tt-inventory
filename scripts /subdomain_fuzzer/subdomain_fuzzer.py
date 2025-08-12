1a6a777218dade39ac926b322d957dc142b13a81
am1tt <eflatmajor811@gmail.com>
2025-08-12 19:38:32 +0530

    [ optimized ] fuzz_subdomain() | returning container for flexibility of functions and future saves | added better error handling

--- scripts /subdomain_fuzzer/subdomain_fuzzer.py
+++ scripts /subdomain_fuzzer/subdomain_fuzzer.py
@@ -1,6 +1,7 @@
 
 import requests
 
+from requests.exceptions import ConnectionError, Timeout, InvalidURL, RequestException
 
 
 def load_subdomains(payload_file):
@@ -12,24 +13,34 @@ def load_subdomains(payload_file):
         print(f"[!] file not found : {payload_file}")
         return []
 
+
+
+def fuzz_subdomain(domain,subdomain): 
+    """ Check if a subdomain is alive and return the status"""
+
+    protocols = ["http","https"]
+
+    results = []
+
+
+    for protocol in protocols:
+        url = f"{protocol}://{subdomain}.{domain}"
+
+        try:
+            response = requests.get(url,timeout=3)
+            results.append(f"[+] {url} - Status : {response.status_code}")
+        except ConnectionError: 
+            results.append(f"[-] {url} - Failed to Connect")
+        except TimeoutError:
+            results.append(f"[-] {url} - Request timed out")
+        except InvalidURL: 
+            results.append(f"[-] {url} - Invalid URL")
+        except RequestException as e:
+            results.append(f"[-] {url} - Request error : {e} ")
     
+    return results
 
-def check_subdomains(domain,payload):
 
-            for sub in subdomains: 
-                for protocol in ["http","https"]: 
 
-                    url = f"{protocol}://{sub}.{domain}"
 
-                    try:
-                        response = requests.get(url,timeout=2)
-                        print(f"[+] {url} - status : {response.status_code}")
-                    except requests.ConnectionError:
-                        print(f"[-] {url} - failed to connect") 
-                    except requests.Timeout: 
-                        print(f"[-] {url} - Request timed out")
-    
 
-domain = "google.com"
-
-check_subdomains(domain,"subdomain.txt")
