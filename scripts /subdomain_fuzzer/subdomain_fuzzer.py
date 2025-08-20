d82314643146110dd5aa98a40a4e0bdb430ed5cf
am1tt <eflatmajor811@gmail.com>
2025-08-20 17:55:45 +0530

    [ optimized ] added ThreadPoolExecutor | satifsying fuzz_subdomains() arguements with futures

--- scripts /subdomain_fuzzer/subdomain_fuzzer.py
+++ scripts /subdomain_fuzzer/subdomain_fuzzer.py
@@ -1,8 +1,10 @@
 
 import requests
 
+
 from requests.exceptions import ConnectionError, Timeout, InvalidURL, RequestException
 
+from concurrent.futures import ThreadPoolExecutor
 
 def load_subdomains(payload_file):
     try:
@@ -31,7 +33,7 @@ def fuzz_subdomain(domain,subdomain):
             results.append(f"[+] {url} - Status : {response.status_code}")
         except ConnectionError: 
             results.append(f"[-] {url} - Failed to Connect")
-        except TimeoutError:
+        except Timeout:
             results.append(f"[-] {url} - Request timed out")
         except InvalidURL: 
             results.append(f"[-] {url} - Invalid URL")
@@ -41,6 +43,26 @@ def fuzz_subdomain(domain,subdomain):
     return results
 
 
+def run_scanner(domain,payload_file,threads=10): 
+    """subdomain fuzzer"""
+
+    subdomains = load_subdomains(payload_file)
+
+    if not subdomains: 
+        return
+    
+
+    with ThreadPoolExecutor(max_workers=threads) as executor: 
+        futures = [executor.submit(fuzz_subdomain,domain,sub) for sub in subdomains]
+
+        for future in futures:
+            for result in future.result():
+                print(result)
 
 
 
+if __name__ == "__main__": 
+    domain = "example.com"
+    payload_file = "subdomain.txt"
+
+    run_scanner(domain,payload_file,threads=10) 
